2b59aa88d05ac1ffdd6fd91b5dbd68dddf03bf13
ZhiAnYu <zhidkovaayu@gmail.com>
2026-04-06 22:37:19 +0400

    Elka

--- src/main/java/ru/netology/demoDocker/DemoDockerApplication.java
+++ src/main/java/ru/netology/demoDocker/DemoDockerApplication.java
@@ -1,15 +1,21 @@
 package ru.netology.demoDocker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @SpringBootApplication
 public class DemoDockerApplication {
-@Value("${inst.num:1}")
+    private static final Logger log = LoggerFactory.getLogger(DemoDockerApplication.class);
+
+@Value("${inst.num:3}")
     private int num;
 
     public static void main(String[] args) {
@@ -18,6 +24,7 @@ public class DemoDockerApplication {
 
     @GetMapping("/")
     public String hello() {
+        log.info("Получен GET запрос на / | instance: {}", num);
         return "Hello from instance " + num + "Ver.2";
     }
 
